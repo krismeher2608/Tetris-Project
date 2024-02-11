95786e60a9eab6ef148b6f5a38bca798938f72b0
krismeher2608 <krishnameher.perumalla@gmail.com>
2024-02-10 22:27:02 -0600

    Adding different folder name in s3 bucket for EKS

--- Jenkins-CICD/backend.tf
+++ Jenkins-CICD/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = var.bucket_name
     key            = "my-terraform-environment/main"
-    region         = var.aws_region
+    region         = "us-east-1"
     dynamodb_table = var.dynamodb_table
   }
 }
