1bfe41ee9fc423683a778f06f19feeeb531dbc6a
krismeher2608 <krishnameher.perumalla@gmail.com>
2024-02-10 21:20:43 -0600

    Updated Instance location to us-east-2 and also the ami of the instance

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,9 +59,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
-  instance_type          = "t2.medium"
-  key_name               = "purplehaze"
+  ami                    = "ami-05fb0b8c1424f266b"
+  instance_type          = "t2.large"
+  key_name               = "Tetris"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
